56dcf1cd7e28210b70debc8e1c670b888a54b0eb
zain2703 <zainnawaz2703@gmail.com>
2020-09-21 13:08:20 +0200

    Final version but used numphy once A1

--- A1.py
+++ A1.py
@@ -2,89 +2,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 ###Assignment1
 import csv
+
+#question2 converting reading into voltages
 def refactor(rf,rows):
-# print (rf)
- # den=2**(10-1)*0.6
- # print (den)
   for i in range(len(rows)):
     for j in range(len(rows[i])):
-#        print(i,j) 
-#        print (rows[i][j])
-#        print (rf[i][j])
-#      print(rf[i][j])
         rf[i][j]= int(rows[i][j]) / (2**10-1)*0.6
 
+#Question3 Compute the mean
 def mean(rf,avg):
    for i in range(len(rows)):
     for j in range(10):
       avg[i]=rf[i][j]+avg[i]
     avg[i]=avg[i]/10
 
+#Question3 subtract the mean
 def sub(rf,avg):
   for i in range(len(rows)):
     for j in range(len(rows[i])):
       rf[i][j]=avg[i]- rf[i][j]
 
 
-## need to add the formula and stroe in same place
-
 filename = "pulses.csv"
-Drows,Dcols =(1000,56)
 fields = [] 
 rows = []
-C1=[]
-# reading csv file 
+# Question1 reading csv file 
 with open(filename, 'r') as csvfile: 
-    # creating a csv reader object 
-  #  csvreader = csv.reader(csvfile, delimiter=",") 
     csvreader = csv.reader(csvfile)
     for i in csvreader:
-     #print (i)
-     #   print (i[0])
+#Seperating time step and the readings
      fields.append(i[0])
      rows.append(i[2:56])
-      #  C1[i]=i[0]
-    # extracting field names through first row 
-   # fields = next(csvreader) 
-  
-    # extracting each data row one by one 
-   #  for row in csvreader: 
-   #     rows.append(row[2:56]) 
-    # get total number of rows 
-#print("Total no. of rows: %d \n"%(csvreader.line_num)) 
-#print(rows)
-#print(rows[3][4])
+
 r=len(rows)
 c=len(rows[0])
-print(c) 
-print(r)
-#rf=[]
 rf = [[0 for i in range(c)] for j in range(r)]
-#for i in range(r-1):
-#    for j in range(c-1):
-#      rf[r][c]=0
-#print(rf)
-#print(fields)
 
+#Question 2 #Function to convert reading into Volatges
 refactor(rf,rows)
-#print(rf)
-###Taking mean
+
+#Question 3 #INitilizing the avg array with zero
 avg=[0 for i in range(r)]
 t= np.arange(0.,54.,1)
 mean(rf,avg)
 
-print(avg)
 # naming the x axis 
 plt.xlabel('x - axis') 
 # naming the y axis 
 plt.ylabel('Voltages') 
 # giving a title to my graph 
 plt.title('Pulse of single row!') 
-# printing the field names 
-#x1 = [1,2,3] 
-#y1 = [2,4,1]
-#plt.subplot(1,2,1)
-plt.plot(t, rf[0], label = "line 1") 
+plt.plot(t, rf[0]) 
 plt.savefig("Pusle.png")
 plt.show()
 sub(rf,avg)
@@ -94,11 +62,9 @@ plt.xlabel('x - axis')
 plt.ylabel('Voltages') 
 # giving a title to my graph
 plt.title('After Refactoring!') 
-#plt.subplot(1,2,2)
-plt.plot(t, rf[0], label = "line 2")
+plt.plot(t, rf[0])
 plt.savefig("Refactor.png")
 plt.show()
-#print('Field names are:' + ', '.join(field for field in fields)) 
 
 
 
