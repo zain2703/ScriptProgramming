22d3b18166455ffbf812dd8fec8b4ce680c43549
zain2703 <zainnawaz2703@gmail.com>
2020-10-14 11:06:31 +0200

    Python tasks till 5 are done

--- MainProject/project.py
+++ MainProject/project.py
@@ -1,14 +1,34 @@
 import sequence
-
+import matplotlib.pyplot as plt
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 # Create an instance of the Sequence class.
-example_sequence1 = sequence.Sequence('AGCT')
+example_sequence1 = sequence.Sequence('AGCTAATA')
 
 # Call the first_base method on our instance. Notice that we don't specify the 'self' parameter when we call the method.
 example_first_base = example_sequence1.first_base()
 print(f'The first base is: {example_first_base}')
 
 
-# TODO add method calls to complete the tasks
+# Task 1
+example_sequence2 = sequence.Sequence('AAGTAATA')
+example1_first_base = example_sequence2.first_base()
+print(f'Task 1: The first base by : {example1_first_base}')
+
+# Task 2
+example_sequence2_base_count = example_sequence2.bases_in_DNA()
+print(f'Task 2: Number of basses of sequence in Task1: {example_sequence2_base_count}')
+
+# Task3
+print(f'Task 3: DNA sequence of Task1: {example_sequence2.bool_dna()}')
+
+# Task4
+print(f'Task 4: compliment of Task1 sequence {example_sequence2.bases} is: {example_sequence2.compliment_of_sequence()}')
+
+# Task5
+print(f'Task 5: pair of non-matching bases: {example_sequence1.matching_sequence(example_sequence2.bases)}')
 
 
--- MainProject/sequence.py
+++ MainProject/sequence.py
@@ -9,11 +9,59 @@ class Sequence:
     def __init__(self, bases):
         # store the bases as an attribute.
         self.bases = bases
-
+    # Task 1
     # This is an example method, it returns the first base.
     def first_base(self):
         # We can access the 'bases' for this instance via 'self'.
         result = self.bases[0]
         return result
+    # Task 2
+    def bases_in_DNA(self):
+        counter = 0
+        for base in self.bases:
+            counter = counter +1
+        return counter
+    # Task 3
+    def bool_dna(self):
+        flag =True
+        if len(self.bases) == 0:
+            flag =False
+        else:
+            for i in range(len(self.bases)):
+              if self.bases[i] == 'A' : flag = True
+              elif self.bases[i] == 'T': flag = True
+              elif self.bases[i] == 'C': flag = True
+              elif self.bases[i] == 'G': flag = True 
+              else:
+                  flag = False
+                  break
+        return flag
+    # Task 4
+    def compliment_of_sequence(self):
+        replace = ''
+        for i in range(len(self.bases)):
+              if self.bases[i] == 'A' :
+                  replace = replace + 'T'
+              elif self.bases[i] == 'T': 
+                  replace = replace + 'A'
+              elif self.bases[i] == 'C': 
+                  replace = replace + 'G'
+              elif self.bases[i] == 'G': 
+                  replace = replace + 'c'
+        return replace
 
-    # TODO: add other methods here to complete the tasks.
+    # Task 5
+    def matching_sequence(self, second_sequence):
+        assert len(self.bases) == len(second_sequence), 'Sequence are not having same length' 
+        
+        if len(self.bases) == 0:
+            return -1
+        else:
+            counter = -1
+            for i in range(len(self.bases)):
+                if self.bases[i] == second_sequence[i]:
+                    counter =counter + 1
+                    if (len(self.bases)-1)== counter:
+                        return -1
+                else:
+                    return counter + 1
